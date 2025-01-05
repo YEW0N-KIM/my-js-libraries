628d0de7ec7d679ca29b3b73bed0d94f5a0831e4
yewon_kim <wonwhimsical@gmail.com>
2025-01-05 15:56:07 +0900

    restart : repository

--- 웹개발자과정_새싹/node/database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2/promise")
-
-exports.pool = mysql.createPool({
-    host: "www.farmersblog.co.kr",
-    user: "dummy",
-    port:"3306",
-    password: "123456",
-    database:"KakaotalkDB",
-});
--- 웹개발자과정_새싹/node/src/router/indexRouter.js
+++
@@ -1,7 +0,0 @@
-const indexController = require("../controller/indexController")
-
-exports.indexRouter = function(app){
-    app.get("/users", indexController.getUsers);
-    app.post("/user", indexController.postLogic);
-};
-
